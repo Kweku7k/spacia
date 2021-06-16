ad9d8882de246d2917c5734d84771b581fb0d226
HenAmy <henryamihere77@gmail.com>
2021-06-16 17:58:07 +0000

    Worked on dropdown and counter but counter goes  beyond zero

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,4 +1,10 @@
-import { FaHome, FaTachometerAlt, FaTimes, FaUsers, FaUser, FaFileInvoiceDollar, FaChartBar, FaCog } from 'react-icons/fa'
+import {useState} from 'react'
+import {Link} from 'react-router-dom'
+import { Dropdown, DropdownButton } from 'react-bootstrap';
+
+import { FaHome, FaTachometerAlt, FaTimes, FaUsers, FaUser, FaFileInvoiceDollar, FaChartBar, FaCog, FaChevronDown, FaChevronRight } from 'react-icons/fa'
+
+//Images
 import logo from '../img/logo.png';
 import bill from '../img/Web/Spacia/bill.png';
 import chart from '../img/Web/Spacia/chart.png';
@@ -8,13 +14,20 @@ import property from '../img/Web/Spacia/property.png';
 import profile from '../img/Web/Spacia/profile.png';
 import users from '../img/Web/Spacia/users.png';
 import settings from '../img/Web/Spacia/settings.png';
-import {Link} from 'react-router-dom'
 import report from '../img/Web/Spacia/chart.png';
 
 
 // import dashboard from '../img/Web/Spacia/dashboard.png'
 
 const SideNav = () => {
+    //setStates to handle dropdown
+    const [open, setOpen] = useState(false);
+
+    //function to toggle dropdown
+    const toggle = () => {
+        setOpen(!open);
+    }
+
     const icon ={
         marginRight:10,
         color: '#F85A47',
@@ -24,24 +37,29 @@ const SideNav = () => {
     const navItem = {
         margin:'auto',
         marginLeft:5,
-        marginBottom:0
+        marginBottom:0,
     }
+
+    const dropItem = {
+        display:'flex',
+        flexDirection: 'column'
+    }
+
     return (
         <div className="sideNav col-md-2" >
 
             <div className="navItem">
-            <img src={logo} width={100} alt="LOGO"/>
+                <img src={logo} width={100} alt="LOGO"/>
             </div>
             
             <Link className="link" to="/dashboard">
-            <div className="navItem">
-            <div>
-                <img src={dash} alt="dashboard"/>
-            </div>
-
-                <h6 style={navItem}> Dashboard</h6>
-                
-            </div>
+                <div className="navItem">
+                    <div>
+                        <img src={dash} alt="dashboard"/>
+                    </div>
+                    
+                    <h6 style={navItem}> Dashboard</h6>
+                </div>
             </Link>
 
             <Link className="link" to="/properties">
@@ -54,25 +72,63 @@ const SideNav = () => {
                 </div>
             </Link>
 
-            <Link className="link" to="/startbooking">
-            <div className="navItem">
-                {/* <FaUser style={icon}/> */}
-                <div>
-                <img src={profile} alt="dashboard"/>
-                </div>
+            <button style={{ background: 'none', border: 'none', padding: '0px' }} onClick={toggle}>
+                <Link className="link">
+                    <div className="navItem" style={{ alignItems:'flex-end' }}>
+                        <div>
+                            <img src={profile} alt="dashboard"/>
+                        </div>
+                        <h6 style={navItem}> Booking</h6>
+                        {open && (
+                            <>
+                                <FaChevronDown style={{ marginLeft:'30px'}}/>
+                            </>
+                        )}
+                        {!open && (
+                            <>
+                                <FaChevronRight style={{ marginLeft:'30px'}}/>
+                            </>
+                        )}
+                    </div>
+                </Link>
+                {open && (
+                    <div style={{ display: 'flex', flexDirection: 'column', }}>
+                        <Link style={{ textDecoration: 'none', color: '#F9F9F9' }} to="/startBooking"> Find a Space</Link>
+                        <Link style={{ textDecoration: 'none', color: '#F9F9F9' }}> Orders</Link>
+                        <Link style={{ textDecoration: 'none', color: '#F9F9F9' }}> Approvals</Link>
+                    </div>
+                )}
+            </button>
 
-                <h6 style={navItem}> Booking</h6>
-            </div>
-            </Link>
+            {/* <Dropdown className="navItem">
+                <Dropdown.Toggle style={{ display: 'flex', alignItems: 'center', padding: 0, color: 'white' }} variant="transparent" >
+                    <div style={{display: 'flex', padding: '0px'}}>
+                        <img src={profile} alt="dashboard"/>
+                        <h6 style={navItem}> Booking</h6>
+                    </div>
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                    <div style={dropItem}>
+                        <Link className='link' style={{color:'black'}} to="/startbooking">Find a Space</Link>
+                    </div>
+                    <div style={dropItem}>
+                        <Link className='link' style={{color:'black'}} to="#">Orders</Link>
+                    </div>
+                    <div style={dropItem}>
+                        <Link className='link' style={{color:'black'}} to="#">Approvals</Link>
+                    </div>
+                </Dropdown.Menu>
+            </Dropdown> */}
 
             <Link className="link" to="/users">
-            <div className="navItem">
-            <div>
-                <img src={users} alt="users" />
-            </div>
-            {/* <img src={profile} width={'100%'}alt="LOGO"/> */}
-                <h6 style={navItem}> Users</h6>
-            </div>
+                <div className="navItem">
+                    <div>
+                        <img src={users} alt="users" />
+                    </div>
+                {/* <img src={profile} width={'100%'}alt="LOGO"/> */}
+                    <h6 style={navItem}> Users</h6>
+                </div>
             </Link>
 
             <Link className="link" to="/billing">
