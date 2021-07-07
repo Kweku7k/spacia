16707b60e4e2d36fecb1fa7270c1b2c6d33ccff6
mr.adumatta@gmail.com <mr.adumatta@gmail.com>
2021-07-07 07:23:01 +0000

    up

--- src/Screens/ListPropertyForm.js
+++ src/Screens/ListPropertyForm.js
@@ -1,9 +1,34 @@
 import React, {useState} from 'react'
 import Header from '../components/Header'
-import { Container, Tabs, Tab } from 'react-bootstrap';
+import { Container, Tabs, Tab, Row } from 'react-bootstrap';
+import uploadButton from '../img/Web/Spacia/Group 426.png'
+import imgplaceholder from '../img/Web/Spacia/imgplaceholder.png'
 
 const ListPropertyForm = () => {
-    const [key, setKey] = useState('home');
+
+  const [image, setImage] = useState(imgplaceholder)
+
+
+  const changeImage = (e) => {
+    // const temp = e.target.value
+    console.log(e.target.src)
+    setImage(e.target.src)
+}
+  const fit = {
+    display:'flex',
+    // justifyContent: 'space-evenly',
+    padding:20
+}
+
+const imgholder = {
+  width:'90%'
+}
+
+const imgholderActive = {
+    border: '1px solid red'
+}
+
+    const [key, setKey] = useState('3.Location');
     return (
         <div>
             <Header title="List a Property"/>
@@ -15,7 +40,7 @@ const ListPropertyForm = () => {
           onSelect={(k) => setKey(k)}
         >
    
-          <Tab eventKey="home" title="Home">
+          <Tab eventKey="1.Description" title="1.Description">
               <br/>
             <Container>
             <h5><b>Property Description</b></h5>
@@ -28,11 +53,356 @@ const ListPropertyForm = () => {
             </div>
             </Container>
           </Tab>
-          <Tab eventKey="profile" title="Profile">
-            <p>this is profile tab</p>
+          <Tab eventKey="2.Media" title="2.Media">
+            
+
+          {/* Upload Box */}
+
+            <h6>Upload Pictures of your property</h6>
+            <div className="uploadBox">
+              <div>
+              <img src={uploadButton} alt="upload" />
+              </div>
+            <button className="uploadButton">Select Media</button>
+            </div>
+
+            {/* placeholders */}
+
+<div style={fit}>
+                <div>
+                    <img onClick={changeImage} src={imgplaceholder} style={imgholderActive} alt="placeholder" />
+                </div>
+                <div>
+                    <img onClick={changeImage} src={imgplaceholder} style={imgholder} alt="placeholder" />
+                </div>
+                <div>
+                    <img onClick={changeImage} src={imgplaceholder} style={imgholder} alt="placeholder" />
+                </div>
+                <div>
+                    <img onClick={changeImage} src={imgplaceholder} style={imgholder} alt="placeholder" />
+                </div>
+                <div>
+                    <img onClick={changeImage} src={imgplaceholder} style={imgholder} alt="placeholder" />
+                </div>
+                <div>
+                    <img onClick={changeImage} src={imgplaceholder} style={imgholder} alt="placeholder" />
+                </div>
+            </div>
+
+
+            <h6>Upload Pictures of your property</h6>
+            <div className="uploadBox">
+              <div>
+              <img src={uploadButton} alt="upload" />
+              </div>
+            <button className="uploadButton">Select Media</button>
+            </div>
+
+            {/* placeholders */}
+
+<div style={fit}>
+                <div>
+                    <img onClick={changeImage} src={imgplaceholder} style={imgholderActive} alt="placeholder" />
+                </div>
+                <div>
+                    <img onClick={changeImage} src={imgplaceholder} style={imgholder} alt="placeholder" />
+                </div>
+                <div>
+                    <img onClick={changeImage} src={imgplaceholder} style={imgholder} alt="placeholder" />
+                </div>
+            </div>
+
+            <div class="row">
+
+            <div class="col-md-6">
+
+            <div class="form-group">
+              <label for="">Image Title</label>
+              <input type="text"
+                class="form-control" name="" id="" aria-describedby="helpId" placeholder="" />
+            </div>
+
+            <div class="form-group">
+              <label for="">Image Description</label>
+              <input type="text"
+                class="form-control" name="" id="" aria-describedby="helpId" placeholder="" />
+            </div>
+
+            <h6><b>Video Option (Optional)</b></h6>
+            <div class="form-group">
+              <label for="">Upload From</label>
+              <input type="text"
+                class="form-control" name="" id="" aria-describedby="helpId" placeholder=""/>
+            </div>
+
+            <div class="form-group">
+              <label for="">Embeded Video ID</label>
+              <input type="text"
+                class="form-control" name="" id="" aria-describedby="helpId" placeholder=""/>
+            </div>
+            </div>
+            </div>
+
           </Tab>
-          <Tab eventKey="setting" title="Setting">
-            <p>this is setting tab</p>
+          
+          
+          <Tab eventKey="3.Location" title="3.Location">
+
+            <h6><b>Property Listing</b></h6>
+
+            <div class="form-group">
+              <label for="">Address</label>
+              <input type="text"
+                class="form-control" name="" id="" aria-describedby="helpId" placeholder="" />
+            </div>
+
+            <Row>
+
+            <div class="col-md-6">
+              <div class="form-group">
+                <label for="">City</label>
+                <input type="text"
+                  class="form-control" name="" id="" aria-describedby="helpId" placeholder="" />
+              </div>
+            </div>
+
+            <div class="col-md-6">
+              <div class="form-group">
+                <label for="">Region Of Province</label>
+                <input type="text"
+                  class="form-control" name="" id="" aria-describedby="helpId" placeholder="" />
+              </div>
+            </div>
+            </Row>
+
+            <Row>
+
+            <div class="col-md-6">
+              <div class="form-group">
+                <label for="">Digital Address</label>
+                <input type="text"
+                  class="form-control" name="" id="" aria-describedby="helpId" placeholder="" />
+              </div>
+            </div>
+
+            <div class="col-md-6">
+              <div class="form-group">
+                <label for="">Select Country</label>
+                <input type="text"
+                  class="form-control" name="" id="" aria-describedby="helpId" placeholder="" />
+              </div>
+            </div>
+            </Row>
+
+            <Row>
+
+            <div class="col-md-6">
+              <div class="form-group">
+                <label for="">Latitude</label>
+                <input type="text"
+                  class="form-control" name="" id="" aria-describedby="helpId" placeholder="" />
+              </div>
+            </div>
+
+            <div class="col-md-6">
+              <div class="form-group">
+                <label for="">Longitude</label>
+                <input type="text"
+                  class="form-control" name="" id="" aria-describedby="helpId" placeholder="" />
+              </div>
+            </div>
+            </Row>
+
+            <div style={{width:'100%', height:'30vh', backgroundColor:'red'}}>
+
+            </div>
+<br/>
+            <div style={{display:'flex'}}>
+              <button className="button-calm">Back</button>
+              <button style={{margin:'auto 20'}} className="button">Next Step</button>
+            </div>
+
+          </Tab>
+          <Tab eventKey="4.Amenities" title="4.Amenities">
+            <h6>Amenities</h6>
+
+            <h6><b>Interior Details</b></h6>
+            
+            <Row>
+            <div class="col-md-4">
+              <div class="form-check">
+                <label class="form-check-label">
+                  <input class="form-check-input" name="" id="" type="checkbox" value="checkedValue" aria-label="Text for screen reader" /> Equiped Kitchen
+                </label>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="form-check">
+                <label class="form-check-label">
+                  <input class="form-check-input" name="" id="" type="checkbox" value="checkedValue" aria-label="Text for screen reader" /> Gym
+                </label>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="form-check">
+                <label class="form-check-label">
+                  <input class="form-check-input" name="" id="" type="checkbox" value="checkedValue" aria-label="Text for screen reader" /> Laundry
+                </label>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="form-check">
+                <label class="form-check-label">
+                  <input class="form-check-input" name="" id="" type="checkbox" value="checkedValue" aria-label="Text for screen reader" /> Media Room
+                </label>
+              </div>
+            </div>
+            </Row>
+
+            <br/>
+            <br/>
+
+            <h6><b>Outdoor Details</b></h6>
+
+            <Row>
+            <div class="col-md-4">
+              <div class="form-check">
+                <label class="form-check-label">
+                  <input class="form-check-input" name="" id="" type="checkbox" value="checkedValue" aria-label="Text for screen reader" /> Back yard
+                </label>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="form-check">
+                <label class="form-check-label">
+                  <input class="form-check-input" name="" id="" type="checkbox" value="checkedValue" aria-label="Text for screen reader" /> Basketball Court
+                </label>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="form-check">
+                <label class="form-check-label">
+                  <input class="form-check-input" name="" id="" type="checkbox" value="checkedValue" aria-label="Text for screen reader" /> Front yard
+                </label>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="form-check">
+                <label class="form-check-label">
+                  <input class="form-check-input" name="" id="" type="checkbox" value="checkedValue" aria-label="Text for screen reader" /> Garage Attatched
+                </label>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="form-check">
+                <label class="form-check-label">
+                  <input class="form-check-input" name="" id="" type="checkbox" value="checkedValue" aria-label="Text for screen reader" /> Hot Bath
+                </label>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="form-check">
+                <label class="form-check-label">
+                  <input class="form-check-input" name="" id="" type="checkbox" value="checkedValue" aria-label="Text for screen reader" /> Pool
+                </label>
+              </div>
+            </div>
+            </Row>
+
+            
+
+            <br/>
+            <br/>
+
+            <h6><b>Utilities</b></h6>
+
+            <Row>
+            <div class="col-md-4">
+              <div class="form-check">
+                <label class="form-check-label">
+                  <input class="form-check-input" name="" id="" type="checkbox" value="checkedValue" aria-label="Text for screen reader" />Central Air
+                </label>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="form-check">
+                <label class="form-check-label">
+                  <input class="form-check-input" name="" id="" type="checkbox" value="checkedValue" aria-label="Text for screen reader" /> Electricity
+                </label>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="form-check">
+                <label class="form-check-label">
+                  <input class="form-check-input" name="" id="" type="checkbox" value="checkedValue" aria-label="Text for screen reader" /> Heating
+                </label>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="form-check">
+                <label class="form-check-label">
+                  <input class="form-check-input" name="" id="" type="checkbox" value="checkedValue" aria-label="Text for screen reader" /> Natural Gas
+                </label>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="form-check">
+                <label class="form-check-label">
+                  <input class="form-check-input" name="" id="" type="checkbox" value="checkedValue" aria-label="Text for screen reader" /> Ventilation
+                </label>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="form-check">
+                <label class="form-check-label">
+                  <input class="form-check-input" name="" id="" type="checkbox" value="checkedValue" aria-label="Text for screen reader" /> Water
+                </label>
+              </div>
+            </div>
+            </Row>
+
+            <br/>
+            <br/>
+
+            <h6><b>Other Features</b></h6>
+
+            <Row>
+            <div class="col-md-4">
+              <div class="form-check">
+                <label class="form-check-label">
+                  <input class="form-check-input" name="" id="" type="checkbox" value="checkedValue" aria-label="Text for screen reader" />Water Sprinklers
+                </label>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="form-check">
+                <label class="form-check-label">
+                  <input class="form-check-input" name="" id="" type="checkbox" value="checkedValue" aria-label="Text for screen reader" /> Elevator
+                </label>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="form-check">
+                <label class="form-check-label">
+                  <input class="form-check-input" name="" id="" type="checkbox" value="checkedValue" aria-label="Text for screen reader" /> Wifi
+                </label>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="form-check">
+                <label class="form-check-label">
+                  <input class="form-check-input" name="" id="" type="checkbox" value="checkedValue" aria-label="Text for screen reader" /> Smoke Detectors
+                </label>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="form-check">
+                <label class="form-check-label">
+                  <input class="form-check-input" name="" id="" type="checkbox" value="checkedValue" aria-label="Text for screen reader" /> Washer and Dryer
+                </label>
+              </div>
+            </div>
+            </Row>
           </Tab>
        
         </Tabs>
